20875f30a59cc49a22d4040d138b4698f8bcce9e
SeidC <SeidelChristof@googlemail.com>
2023-03-20 16:57:34 +0100

    .

--- include/LinkedList.h
+++ include/LinkedList.h
@@ -5,12 +5,17 @@
 
 template <class T> class LinkedList 
 {
+    private: /* Types*/ 
+        struct Data_t{
+            T* data;
+            T* next;
+        };
 
     public: 
         LinkedList() {}
 
 
-
+        Data_t m_data;
 };
 
 
